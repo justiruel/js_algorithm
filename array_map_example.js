a69cae16980320dcf9e8aa5ed918bfb3596a3302
Call Me Anything <tafsirulka@gmail.com>
2017-09-18 12:29:26 +0700

    .

---
+++ array_map_example.js
@@ -0,0 +1,90 @@
+var arr =
+[
+    {
+        "_id": "59bc0d870afaab6849ec5459",
+        "linkfile": "http://snaappcdn.mggsoftware.com/upload/photoblog/cover/20170916002734aselole.jpg",
+        "price": 100,
+        "__v": 0,
+        "updatedAt": "2017-09-15T17:27:35.861Z",
+        "createdAt": "2017-09-15T17:27:35.861Z"
+    },
+    {
+        "_id": "59bc0e0ed1baa968c0369183",
+        "linkfile": "http://snaappcdn.mggsoftware.com/upload/photoblog/cover/20170916002948aselole.jpg",
+        "price": 3000,
+        "__v": 0,
+        "updatedAt": "2017-09-15T17:29:50.410Z",
+        "createdAt": "2017-09-15T17:29:50.409Z"
+    },
+    {
+        "_id": "59bf2b0477fbc616240bec2e",
+        "linkfile": "http://snaappcdn.mggsoftware.com/upload/photoblog/cover/20170918091008aselole.jpg",
+        "price": 5000,
+        "__v": 0,
+        "updatedAt": "2017-09-18T02:10:12.747Z",
+        "createdAt": "2017-09-18T02:10:12.747Z"
+    },
+    {
+        "_id": "59bf2b30c8349d166695d1a4",
+        "linkfile": "http://snaappcdn.mggsoftware.com/upload/photoblog/cover/201709180910553b2",
+        "price": 5000,
+        "__v": 0,
+        "updatedAt": "2017-09-18T02:10:56.538Z",
+        "createdAt": "2017-09-18T02:10:56.537Z"
+    },
+    {
+        "_id": "59bf2b46f6b65a16a8d763ee",
+        "linkfile": "http://snaappcdn.mggsoftware.com/upload/photoblog/cover/201709180911173b2",
+        "price": 5000,
+        "__v": 0,
+        "updatedAt": "2017-09-18T02:11:18.713Z",
+        "createdAt": "2017-09-18T02:11:18.712Z"
+    },
+    {
+        "_id": "59bf2b52f6b65a16a8d763ef",
+        "linkfile": "http://snaappcdn.mggsoftware.com/upload/photoblog/cover/201709180911293b2",
+        "price": 5000,
+        "__v": 0,
+        "updatedAt": "2017-09-18T02:11:30.619Z",
+        "createdAt": "2017-09-18T02:11:30.619Z"
+    },
+    {
+        "_id": "59bf2b56f6b65a16a8d763f0",
+        "linkfile": "http://snaappcdn.mggsoftware.com/upload/photoblog/cover/201709180911333b2",
+        "price": 5000,
+        "__v": 0,
+        "updatedAt": "2017-09-18T02:11:34.757Z",
+        "createdAt": "2017-09-18T02:11:34.757Z"
+    },
+    {
+        "_id": "59bf2b5cf6b65a16a8d763f1",
+        "linkfile": "http://snaappcdn.mggsoftware.com/upload/photoblog/cover/201709180911383b2",
+        "price": 5000,
+        "__v": 0,
+        "updatedAt": "2017-09-18T02:11:40.048Z",
+        "createdAt": "2017-09-18T02:11:40.048Z"
+    },
+    {
+        "_id": "59bf2b61f6b65a16a8d763f2",
+        "linkfile": "http://snaappcdn.mggsoftware.com/upload/photoblog/cover/201709180911433b2",
+        "price": 5000,
+        "__v": 0,
+        "updatedAt": "2017-09-18T02:11:45.061Z",
+        "createdAt": "2017-09-18T02:11:45.061Z"
+    },
+    {
+        "_id": "59bf2b66f6b65a16a8d763f3",
+        "linkfile": "http://snaappcdn.mggsoftware.com/upload/photoblog/cover/201709180911473b2",
+        "price": 5000,
+        "__v": 0,
+        "updatedAt": "2017-09-18T02:11:50.519Z",
+        "createdAt": "2017-09-18T02:11:50.519Z"
+    }
+]
+
+arr.map(function(key, index) {
+   console.log(key._id);
+});
+/*Object.keys(myObject).map(function(key, index) {
+   myObject[key] *= 2;
+});*/
